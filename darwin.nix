0d0baa3b606fcd1f311f8e4be8cb10dd8952f893
Niccolo Borgioli <hi@nicco.io>
2025-03-10 22:11:57 +0100

    cleanup darwin config

--- darwin.nix
+++ darwin.nix
@@ -20,48 +20,52 @@
   # Nix Darwin
   # https://daiderd.com/nix-darwin/manual/index.html
 
-  # Security
-  system.defaults.screensaver.askForPassword = true;
-  system.defaults.screensaver.askForPasswordDelay = 0;
-  system.defaults.loginwindow.GuestEnabled = false;
+  system.defaults = {
+    # Security
+    screensaver.askForPassword = true;
+    screensaver.askForPasswordDelay = 0;
+    loginwindow.GuestEnabled = false;
 
-  # Dock
-  system.defaults.dock.autohide = true;
-  system.defaults.dock.orientation = "left";
-  system.defaults.dock.show-recents = false;
-  system.defaults.dock.persistent-apps = [
-    "/Applications/Arc.app"
-    "/Applications/Ghostty.app"
-    "/Applications/VSCodium.app"
-    "/Applications/Spotify.app"
-    "/System/Applications/System Settings.app"
-  ];
-  system.defaults.dock.persistent-others = [ ];
+    # Dock
+    dock = {
+      autohide = true;
+      orientation = "left";
+      show-recents = false;
+      persistent-apps = [
+        "/Applications/Arc.app"
+        "/Applications/Ghostty.app"
+        "/Applications/VSCodium.app"
+        "/Applications/Spotify.app"
+        "/System/Applications/System Settings.app"
+      ];
+      persistent-others = [ ];
+    };
+
+    # Finder
+    finder = {
+      AppleShowAllExtensions = true;
+      ShowPathbar = true;
+    };
+
+    # Trackpad
+    NSGlobalDomain = {
+      InitialKeyRepeat = 25;
+      KeyRepeat = 2;
+      "com.apple.mouse.tapBehavior" = 1;
+      "com.apple.trackpad.scaling" = 0.875;
+    };
+    trackpad = {
+      Dragging = true;
+      Clicking = true;
+      TrackpadRightClick = true;
+    };
+  };
 
   # Input devices
   system.keyboard = {
     enableKeyMapping = true;
     remapCapsLockToEscape = true;
-    userKeyMapping = [
-      {
-        # Unbind cmd + space
-        HIDKeyboardModifierMappingSrc = 30064771129; # Command + Space
-        HIDKeyboardModifierMappingDst = 0; # No action (unbind)
-      }
-    ];
   };
-  system.defaults.NSGlobalDomain.InitialKeyRepeat = 25;
-  system.defaults.NSGlobalDomain.KeyRepeat = 2;
-  system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
-  system.defaults.NSGlobalDomain."com.apple.trackpad.scaling" = 0.875;
-  system.defaults.trackpad = {
-    Dragging = true;
-    Clicking = true;
-  };
-
-  # Finder
-  system.defaults.finder.AppleShowAllExtensions = true;
-  system.defaults.finder.ShowPathbar = true;
 
   # Other
   system.startup.chime = false;
@@ -71,6 +75,7 @@
     shell = pkgs.fish;
   };
   programs.fish.enable = true;
+  environment.shells = [ pkgs.fish ];
 
   homebrew = {
     enable = true;
